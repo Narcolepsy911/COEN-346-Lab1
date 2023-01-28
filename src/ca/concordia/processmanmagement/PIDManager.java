6433e0ae8c5fcdd9aa7c67acf1afc9b929ff57f3
Ali M <ali.mahgoub1999@gmail.com>
2023-01-28 12:21:41 -0500

    Initial Commit

---
+++ src/ca/concordia/processmanmagement/PIDManager.java
@@ -0,0 +1,18 @@
+package ca.concordia.processmanmagement;
+
+public class PIDManager {
+    static final int MIN_PID = 300;
+    static final int MAX_PID = 500;
+
+    void allocateMap(){
+
+    }
+
+    int allocatePid(){
+        return 0;
+    }
+
+    void releasePid(int pid) {
+
+    }
+}
